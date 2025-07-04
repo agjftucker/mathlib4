02ed817c28d49ab705193c4ca7e4687ce073db38
Yaël Dillies <yael.dillies@gmail.com>
2025-07-04 11:37:33 +0000

    fix

--- Mathlib/Topology/Algebra/Valued/WithZeroMulInt.lean
+++ Mathlib/Topology/Algebra/Valued/WithZeroMulInt.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Salvatore Mercuri
 -/
 import Mathlib.Topology.Algebra.Valued.ValuationTopology
-import Mathlib.Algebra.GroupWithZero.Int
 
 /-!
 # Topological results for integer-valued rings
@@ -15,29 +14,33 @@ is a `Valued R ℤₘ₀` instance but no canonical base with which to embed thi
 `NNReal`.
 -/
 
-open Multiplicative WithZero WithZeroMulInt
-
+open WithZero
 open scoped Topology
 
 namespace Valued.WithZeroMulInt
 
-variable {R : Type*} [Ring R] [Valued R ℤₘ₀]
+variable {R : Type*} [Ring R] [Valued R ℤᵐ⁰] {x : R}
 
 open Set Filter in
-/-- In a `ℤₘ₀`-valued ring, powers of `x` tend to zero if `v x ≤ ofAdd (-1 : ℤ)`. -/
-theorem tendsto_zero_pow_of_le_neg_one {x : R} (hx : v x ≤ exp (-1 : ℤ)) :
-    Tendsto (fun (n : ℕ) => x ^ n) atTop (𝓝 0) := by
-  simp only [(hasBasis_nhds_zero _ _).tendsto_right_iff, mem_setOf_eq, map_pow, eventually_atTop]
-  refine fun γ _ => ⟨- (log γ - 1) |>.toNat, fun b hb => ?_⟩
-  apply lt_of_le_of_lt (pow_le_pow_left₀ zero_le' hx b) ?_
-  rw [← Units.val_pow_eq_pow_val, exp_pow, ← lt_log_iff_exp_lt]
-  omega
+/-- In a `ℤᵐ⁰`-valued ring, powers of `x` tend to zero if `v x ≤ exp (-1)`. -/
+theorem tendsto_zero_pow_of_le_exp_neg_one (hx : v x ≤ exp (-1)) :
+    Tendsto (fun n : ℕ ↦ x ^ n) atTop (𝓝 0) := by
+  simp only [(hasBasis_nhds_zero _ _).tendsto_right_iff, mem_setOf_eq, map_pow, eventually_atTop,
+    forall_const, logEquiv.forall_congr_left, logEquiv_symm, coe_expEquiv_apply]
+  obtain hvx | hvx := eq_or_ne (v x) 0
+  · exact fun _ ↦ ⟨1, fun n hn ↦ by rw [hvx, zero_pow (by omega)]; exact exp_pos⟩
+  refine fun γ ↦ ⟨(1 - γ).toNat, fun b hb ↦ lt_exp_of_log_lt ?_⟩
+  calc
+        (v x ^ b).log
+    _ = b * (v x).log := by simp [log_pow]
+    _ ≤ b * (-1) := by gcongr; simpa [log_le_iff_le_exp hvx]
+    _ < γ := by omega
 
 open Filter in
-theorem exists_pow_lt_of_le_neg_one {x : R} (hx : v x ≤ exp (-1 : ℤ)) (γ : ℤₘ₀ˣ) :
+theorem exists_pow_lt_of_le_exp_neg_one (hx : v x ≤ exp (-1)) (γ : ℤᵐ⁰ˣ) :
     ∃ n, v x ^ n < γ := by
-  let ⟨n, hn⟩ := eventually_atTop.1 <|
-     (hasBasis_nhds_zero _ _ |>.tendsto_right_iff).1 (tendsto_zero_pow_of_le_neg_one hx) γ trivial
+  let ⟨n, hn⟩ := eventually_atTop.1 <| (hasBasis_nhds_zero _ _ |>.tendsto_right_iff).1
+    (tendsto_zero_pow_of_le_exp_neg_one hx) γ trivial
   exact ⟨n, by simpa using hn n le_rfl⟩
 
 end Valued.WithZeroMulInt
